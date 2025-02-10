7790ea0045176d7b4cf7f004d1db99d6c95ff433
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-02-10 03:15:53 +0100

    commit

---
+++ fizzy-jesus/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import localFont from 'next/font/local'
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import './input.css'
+import Header from '@/components/Header';
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2", // Usa una ruta desde /public
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body className='overflow-x-hidden bg-yellow-300'>
+      <Header/>
+      <main>
+
+      </main>
+      {children}
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
