18102b99fb15561631bca4928008af073e3f4ae9
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-02-12 11:30:16 +0100

    commit

--- fizzy-jesus/src/app/layout.tsx
+++ fizzy-jesus/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { repositoryName } from "@/prismicio";
 
 import './input.css'
 import Header from '@/components/Header';
+import ViewCanvas from '@/components/ViewCanvas';
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2", // Usa una ruta desde /public
@@ -24,7 +25,8 @@ export default function RootLayout({
       <body className='overflow-x-hidden bg-yellow-300'>
       <Header/>
       <main>
-
+      {children}
+      <ViewCanvas/>
       </main>
       {children}
       </body>
--- fizzy-jesus/src/components/Header.tsx
+++ fizzy-jesus/src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { FizziLogo } from '@/components/FizziLogo'
+import React from "react";
+import { FizziLogo } from "@/components/FizziLogo";
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className='flex justify-center py-4 -mb-28'>
-        <FizziLogo className='h-20 z-10 cursor-pointer text-sky-800'/>
+    <header className="-mb-28 flex justify-center py-4">
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
     </header>
-  )
+  );
 }
